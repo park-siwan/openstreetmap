81514bcdff4c5c451411c6f85d8a8e7c687f633b
park-siwan <pswan3168@gmail.com>
2021-05-27 17:46:44 +0900

    맵 import 성공

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
+import React from 'react';
+import { MapContainer, Marker, Popup, TileLayer } from 'react-leaflet';
+import { Icon } from 'leaflet';
+// import * as parkData from './data/skateboard-parks.json';
 import './App.css';
+import LocationMarker from './LocationMarker';
 
-function App() {
+export const icon = new Icon({
+  iconUrl: '/skateboarding.svg',
+  iconSize: [25, 25],
+});
+
+export default function App() {
+  // const [activePark, setActivePark] = React.useState(null);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <MapContainer
+      center={[35.5313429, 129.3057098, 17.86]}
+      zoom={20}
+      scrollWheelZoom={true}
+    >
+      <TileLayer
+        attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      />
+      <Marker position={[35.5313429, 129.3057098, 17.86]}>
+        <Popup>
+          A pretty CSS3 popup. <br /> Easily customizable.
+        </Popup>
+      </Marker>
+      {/*현재위치를 찾아주는 기능*/}
+      {/* <LocationMarker />  */}
+    </MapContainer>
   );
 }
-
-export default App;
